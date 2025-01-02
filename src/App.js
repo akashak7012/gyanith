3f4380cc4b2cb1dd7f363d69a7df066b42ac8201
akashak7012 <akashak7012@gmail.com>
2025-01-02 13:04:56 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Content from './Content';
+
+
+function App() {
+  return (
+   <Content></Content>
+
+  
+  );
+}
+
+export default App;
